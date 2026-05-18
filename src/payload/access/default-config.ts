79890b278ac1e421c83cf0c9b67cee4647df33ee
Pieter du Toit <pdut8901@gmail.com>
2026-05-18 11:04:48 +0200

    refactor: scaffold clean nextjs site

--- src/payload/access/default-config.ts
+++
@@ -1,9 +0,0 @@
-import { isAdmin } from './is-admin'
-import { isLoggedIn } from './is-logged-in'
-
-export const DEFAULT_COLLECTION_ACCESS = {
-  read: isLoggedIn,
-  create: isLoggedIn,
-  update: isLoggedIn,
-  delete: isAdmin
-}
--- src/payload/utils/get-domain.ts
+++
@@ -1,8 +0,0 @@
-export function getDomain(): string {
-  const domain =
-    process.env.VERCEL_PROJECT_PRODUCTION_URL ||
-    process.env.VERCEL_URL ||
-    process.env.VERCEL_BRANCH_URL
-
-  return domain || 'localhost:3000'
-}
